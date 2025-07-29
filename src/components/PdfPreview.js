40a78157e4f3a33917a89020afb1ededef713cf3
Bana Yugandhar Reddy <157593298+BanaYugandharReddy08@users.noreply.github.com>
2025-07-29 17:39:17 +0100

    Replace pdf viewer with react-pdf

--- src/components/PdfPreview.js
+++ src/components/PdfPreview.js
@@ -1,16 +1,23 @@
-import PDFViewer from 'pdf-viewer-reactjs';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 function PdfPreview({ url }) {
+  const [numPages, setNumPages] = useState(null);
+
+  const onLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
-    <PDFViewer
-      document={{
-        url: url // This can be a Blob URL or a remote URL
-      }}
-      hideZoom={true}
-      hideRotation={true}
-      hideNavbar={false}
-      css="custom-pdf-viewer"
-    />
+    <div className="pdf-viewer">
+      <Document file={url} onLoadSuccess={onLoadSuccess} data-testid="pdf-document">
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page key={`page_${index + 1}`} pageNumber={index + 1} data-testid="pdf-page" />
+        ))}
+      </Document>
+    </div>
   );
 }
 
